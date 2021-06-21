84d3b567682b5d1d3ffa467f575f16c08e9dab2e
Luis Castillo <mr.luiscastillo@gmail.com>
2021-06-20 23:30:35 -0400

    commit tree 063923611c90f144424e05b7b8f4dde28221f36c

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+let dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px"
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10)
+    if (left < 360 ) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+function moveDodgerUp(){
+    let upNumbers = dodger.style.top.replace("px", "")
+    let up = parseInt(upNumbers, 10)
+    if (up <= 380){
+        dodger.style.top = `${up - 1}px`
+    }
+
+}
+
+document.addEventListener("keydown", function(event){
+    if (event.key == "ArrowLeft"){
+        moveDodgerLeft()
+    }
+    if (event.key == "ArrowRight"){
+        moveDodgerRight()
+    }
+    if (event.key == "ArrowUp"){
+        moveDodgerUp()
+    }
+})
